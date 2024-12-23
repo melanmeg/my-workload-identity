80197bf1916b2bc789d31c4500f513498e14cc94
melanmeg <naoya05280708@gmail.com>
2024-12-23 23:17:11 +0900

    f

--- my/locals.tf
+++ my/locals.tf
@@ -3,6 +3,8 @@ locals {
   region     = "asia-northeast1"
   zones      = ["asia-northeast1-a"]
 
+  wip_project_id = "wip-project-melanmeg"
+
   repository = "melanmeg/my-workload-identity"
   service_account_name = "terraform@wip-project-melanmeg.iam.gserviceaccount.com"
   workload_identity_pool_name = "projects/502820203034/locations/global/workloadIdentityPools/github-pool"
--- my/main.tf
+++ my/main.tf
@@ -5,7 +5,7 @@
 # }
 
 resource "google_service_account_iam_member" "owner" {
-  service_account_id = "projects/${local.project_id}/serviceAccounts/${local.service_account_name}"
+  service_account_id = "projects/${local.wip_project_id}/serviceAccounts/${local.service_account_name}"
   role               = "roles/owner"
   member             = "principalSet://iam.googleapis.com/${local.workload_identity_pool_name}/attribute.repository/${local.repository}"
 }
