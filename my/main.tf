f8af2b1d6c3ce38e99e885e277180945bbb6a615
melanmeg <naoya05280708@gmail.com>
2024-12-23 21:52:19 +0900

    fix

--- my/main.tf
+++ my/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "example" {
-  name          = "example-bucket-melanmeg-my-1"
+  name          = "example-bucket-melanmeg-my-1-tmp1"
   location      = "US"
   force_destroy = true
 }
