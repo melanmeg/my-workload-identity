9e71bf80bdfb1b92188345ddbfc3893aa23a27c0
melanmeg <naoya05280708@gmail.com>
2024-12-23 23:24:29 +0900

    f

--- my/main.tf
+++ my/main.tf
@@ -1,8 +1,11 @@
-# resource "google_storage_bucket" "example" {
-#   name          = "example-bucket-melanmeg-my-1-tmp5"
-#   location      = "US"
-#   force_destroy = true
-# }
+resource "google_storage_bucket" "example" {
+  name          = "tfstate-bucket-melanmeg-my-1"
+  location      = "US"
+
+  uniform_bucket_level_access = true
+
+  force_destroy = true
+}
 
 resource "google_service_account_iam_member" "owner" {
   service_account_id = "projects/${local.wip_project_id}/serviceAccounts/${local.service_account_name}"
