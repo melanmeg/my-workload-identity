74ee9d8922a636eeb5016db7c30b7850f2b35527
melanmeg <naoya05280708@gmail.com>
2025-01-01 01:48:05 +0900

    fix

---
+++ terraform/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  project_id = "wip-project-melanmeg"
+  region     = "asia-northeast1"
+  zones      = ["asia-northeast1-a"]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,21 @@
-resource "google_iam_workload_identity_pool" "github_pool" {
+resource "google_iam_workload_identity_pool" "my_pool" {
   project                   = local.project_id
-  workload_identity_pool_id = local.workload_identity_pool_id
+  workload_identity_pool_id = "my-pool"
 }
 
+############################################
+# GitHub Pool Provider                     #
+############################################
 resource "google_iam_workload_identity_pool_provider" "github_pool_provider" {
   project                            = local.project_id
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
-  workload_identity_pool_provider_id = local.workload_identity_pool_provider_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.my_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-pool-provider"
   attribute_mapping = {
     "google.subject"             = "assertion.sub"
     "attribute.repository"       = "assertion.repository"
     "attribute.repository_owner" = "assertion.repository_owner"
   }
-  attribute_condition = "assertion.repository == '${local.repository}'"
+  attribute_condition = "assertion.repository == 'melanmeg/my-workload-identity' || assertion.repository == 'melanmeg/my-k8s-app'"
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
@@ -25,7 +28,10 @@ resource "google_service_account" "github" {
 resource "google_service_account_iam_member" "workload_identity_member" {
   service_account_id = google_service_account.github.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.repository}"
+  members            = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.my_pool.name}/attribute.repository/melanmeg/my-workload-identity",
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.my_pool.name}/attribute.repository/melanmeg/my-k8s-app",
+  ]
 }
 
 # resource "google_service_account_iam_member" "owner" {
@@ -34,8 +40,8 @@ resource "google_service_account_iam_member" "workload_identity_member" {
 #   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.repository}"
 # }
 
-# resource "google_project_iam_member" "owner" {
-#   project = "my-project-melanmeg"
-#   role    = "roles/owner"
-#   member  = "serviceAccount:${google_service_account.github.email}"
-# }
+resource "google_project_iam_member" "owner" {
+  project = "my-project-melanmeg"
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.github.email}"
+}
